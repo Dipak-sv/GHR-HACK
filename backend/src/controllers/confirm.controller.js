08530827f4bf4ec864fd6a1d82bf6e14fb4495cb
purvaahirrao <purvaahirrao02@gmail.com>
2026-02-28 15:10:11 +0530

    implent the confirm route and controller

--- backend/src/controllers/confirm.controller.js
+++ backend/src/controllers/confirm.controller.js
@@ -1,8 +1,23 @@
+
+const Prescription = require("../models/prescription.model");
+
 exports.confirmPrescription = async (req, res, next) => {
     try {
+        const { id } = req.params;
+
+        const prescription = await Prescription.findById(id);
+        if (!prescription) {
+            return res.status(404).json({ message: "Prescription not found" });
+        }
+
+        prescription.status = "confirmed";
+        const pre=await prescription.save();
+        //  res.send(pre);
         res.status(200).json({
-            message: "Confirm!!"
+            message: "Prescription confirmed successfully",
+            data: prescription
         });
+        
     } catch (err) {
         next(err);
     }
