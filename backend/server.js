ce79db5fd2ee4eda46aac87ae76353a26630b86e
purvaahirrao <purvaahirrao02@gmail.com>
2026-02-28 12:17:07 +0530

    error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const app = require("./src/app");
-const mongoose=require("mongoose");
+
 const connectDB = require("./src/config/db");
 
 const PORT = process.env.PORT || 3000;
--- backend/src/app.js
+++ backend/src/app.js
@@ -16,12 +16,22 @@ const uploadRoutes = require("./routes/upload.route");
 const prescriptionRoutes = require("./routes/prescription.route");
 const simplifyRoutes = require("./routes/simplify.route");
 const confirmRoutes = require("./routes/confirm.route");
+const errorHandler = require("./middleware/error.middleware");
+
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/upload", uploadRoutes);
 app.use("/api/prescription", prescriptionRoutes);
 app.use("/api/simplify", simplifyRoutes);
 app.use("/api/confirm", confirmRoutes);
+
+
+app.use((req, res, next) => {
+    res.status(404).json({ success: false, message: "Route not found" });
+});
+
+app.use(errorHandler);
 
 module.exports = app;
